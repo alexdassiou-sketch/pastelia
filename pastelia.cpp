81d3cacad111b87d8fde4fb1ecafd2013471d2a6
aleks <alexdassiou@gmail.com>
2026-04-17 15:45:12 +0300

    main upload
    Added user input for sesame and honey quantities.

--- pastelia.cpp
+++ pastelia.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main(){
-cout<< "Geia alex" <<endl;
-  cout<<"heloo"<<endl;
-  cout<< "Opa bro" << endl;
+
+  int wres, tempMalako=120, tempSkliro=125;
+  double posotitaSousami, posotitaMeli, perissevmaSousami=0.0, perissevmaMeli=0.0;
+  //Ζητάμε από τον χρήστη να εισάγει τις ποσότητες του σουσαμιού και του μελιού σε kg και τις αποθηκεύουμε στο posotitaSousami και posotitaMeli αντίστοιχα.//
+  //Οι ώρες αφορούν τον χρόνο ψησίματος του παστελιού.//
+  //Το tempMalako και tempSkliro αφορoύν τις θερμοκρασίες ψησίματος για το μαλακό και σκληρό παστέλι αντίστοιχα.//
+  //Το perissevmaSousami και PerissevmaMeli αφoρoύν τα υπολείμματα του σουσαμιού και του μελιού σε kg ή γραμμάρια μετά τo ψήσιμo του παστελιoύ.//
+
+  cout << "Βάλε την ποσότητα του σουσαμιού: "<< endl;
+  cin >> posotitaSousami;
+  cout << "Ποσότητα μελιού σε kg: ";
+  cin >> posotitaMeli;
+
+  
 
 }
